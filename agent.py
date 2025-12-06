2b28de1a337d2f4d65073ad75f5aeedaf265b4c7
unfiltered-syrup <jw6058@nyu.edu>
2025-12-06 13:41:24 -0500

    agent tool usage

--- agent.py
+++ agent.py
@@ -3,10 +3,24 @@ from langchain_openai import ChatOpenAI
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
 from datetime import datetime
+from langchain_core.tools import tool
+
+
+
+@tool
+def get_current_time(_: str = "") -> str:
+    """Use this to find out the current date and time."""
+    from datetime import datetime
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+
+CUSTOM_TOOLS = [get_current_time]
+
+
 
 
 def build_steam_db() -> SQLDatabase:
-    db_uri = "sqlite:///steam_top_100x200.db"
+    db_uri = "sqlite:///steam_top_2000x100.db"
     db = SQLDatabase.from_uri(db_uri)
     return db
 
@@ -31,13 +45,15 @@ def build_agent():
         verbose=True,
 
         prefix=(
-            f"You are a Steam game recommendation assistant. Today's date is {date}, "
+            f"You are a Steam game recommendation assistant."
             "You have access to a SQLite database with a table 'steam_games' and 'steam_reviews'"
             "containing information about the top games on steam. "
             "Use SQL to query that table when you need factual data. "
             "Then answer in natural language, citing game names and explaining your reasoning."
-            "Always give examples of comments from players, and summarize the overall sentiment"
+            "Always give examples of comments from players, and summarize the overall sentiment."
+            "When user asks for the latest information, check for current date time"
         ),
+        extra_tools=CUSTOM_TOOLS
     )
     return agent
 
